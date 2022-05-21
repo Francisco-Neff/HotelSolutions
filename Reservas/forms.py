7b62f6019d053631cf4fc3773c22b92fc712e9d0
Francisco Neff <francisconeff@gmail.com>
2022-05-21 11:27:53 +0200

    min value 1 al comprobar disponibilidad

--- Reservas/forms.py
+++ Reservas/forms.py
@@ -9,7 +9,7 @@ class DisponibilidadForm(forms.Form):
     #Formulario para comprobar la disponibilidad, se añade al campo fecha de entrada la fecha actual.
     fx_entrada = forms.DateField(label='Fecha de entrada',initial=datetime.datetime.today().strftime('%d/%m/%Y') ,input_formats=DATE_INPUT_FORMATS,validators=[MaxValueValidator('31/12/2022'),MinValueValidator(datetime.datetime.today().strftime('%d/%m/%Y'))])
     fx_salida = forms.DateField(label='Fecha de salida',input_formats=DATE_INPUT_FORMATS,validators=[MaxValueValidator('31/12/2022'),MinValueValidator(datetime.datetime.today().strftime('%d/%m/%Y'))])
-    huespedes = forms.IntegerField(max_value=4, label='Huespedes')
+    huespedes = forms.IntegerField(min_value=1, max_value=4, label='Huespedes')
     #Nota: Se pone 4 personas como máximo ya que es el tope de una habitación, pero se deberia implementar un método que pregunte al usuario si son
     # mas de 4 como se quiere alojar 
     # Ejemplo, una reserva para 6 personas:
